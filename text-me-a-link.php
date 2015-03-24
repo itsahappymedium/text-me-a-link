5c7fc0a7e91fe3bb0992296bf3090bbcefe13924
Josh Larson <jplhomer@gmail.com>
2015-03-23 21:08:53 -0500

    Initialize plugin

--- text-me-a-link.php
+++ text-me-a-link.php
@@ -6,16 +6,41 @@ Description: Allows users to easily text themselves a link using the Twilio API.
 Author: Happy Medium
 Author URI: http://itsahappymedium.com
 Plugin URI: http://itsahappymedium.com
-Text Domain: text-me-a-link
+Text Domain: tmal
 Domain Path: /languages
 */
 
 if ( ! class_exists( 'TMaL' ) ) :
 
 class TMaL {
-    function __construct() {
 
+    /**
+     * Plugin settings
+     * @var array
+     */
+    var $settings;
+
+    function __construct() {
+        $this->settings = array(
+            'version'       => '0.1-alpha',
+            'name'          => __( 'Text Me a Link', 'tmal' ),
+            'dir'           => trailingslashit( plugin_dir_path( __FILE__ ) ),
+            'url'           => trailingslashit( plugin_dir_url( __FILE__ ) ),
+        );
     }
 }
+
+function tmal() {
+    global $tmal;
+
+    if ( ! isset($tmal) ) {
+        $tmal = new TMaL();
+    }
+
+    return $tmal;
+}
+
+// Initialize
+tmal();
 
 endif;
